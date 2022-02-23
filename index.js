79540d18ab62e3e4fbded4cad9447b7758162c47
shlok1999 <09shlok1999ae1@gmail.com>
2022-02-23 23:42:52 +0530

    mobile responsive

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require("dotenv").config()
 const app = express()
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3001
 
 //Middleware
 const path = require('path')
